7b1e1a5e9edc4de304c30ea1fe4327bb93e2191b
Tatsuhiko Arai <tatsu@tatsu.com>
2016-07-09 16:57:01 +0900

    Add a map

--- GoogleMapsSample/ViewController.swift
+++ GoogleMapsSample/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let camera = GMSCameraPosition.cameraWithLatitude(-37.809487, longitude: 144.965699, zoom: 17.5, bearing: 30, viewingAngle: 40)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        self.view = mapView
     }
 
     override func didReceiveMemoryWarning() {
